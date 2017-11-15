4998f3d042c5e8fea89520010c3842f86956fc06
4015a11 <4015a11@guama.ece.uprm.edu>
2017-11-15 13:00:07 -0400

    Added MyPanelClass which will color the frame and add 2 borders, each of a different color.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,12 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Raymond M Nunez");
+		JFrame myFrame = new JFrame("Raymond Nunez");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		myFrame.setLocationRelativeTo(null);
 		myFrame.setSize(200, 200);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
